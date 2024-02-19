37007308dade038d15f682f25a432581cf37529c
Pryxs <corentin25200@gmail.com>
2024-02-19 11:38:26 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { logo } from "./assets";
+
+import { Button } from "./components";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Button.tsx
@@ -0,0 +1,8 @@
+type ButtonProps = {
+  type?: string;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const Button: React.FC<ButtonProps> = ({ type }) => {
+  return <div>Button</div>;
+};
